3e787bed7a7629c438d8d4b642d635b4819822bd
Akihiko Nakamura <anakamuro@earthlink.net>
2023-03-09 13:32:53 -0500

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,19 @@
 //import logo from './logo.svg';
 //import './App.css';
+import {useNavigate} from "react-router-dom"
+
+
 
 function Main() {
+  const path = useNavigate()
+
+  function Logout(){
+    path('/login')
+}
   return (
     <div className="App">
      Main
+     <button className="signin" onClick={Logout}>Logout</button>
     </div>
   );
 }
