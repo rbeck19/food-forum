f744a77488b141e8bbe701c831f4926c94cb5108
speckoh <speckohart@gmail.com>
2023-03-11 13:03:19 -0500

    linking to details

--- src/pages/RecipeDetailPage/RecipeDetailPage.js
+++ src/pages/RecipeDetailPage/RecipeDetailPage.js
@@ -1,18 +1,9 @@
-//import { checkToken } from "../../utilities/users-service"
+
 
 export default function RecipeDetailPage() {
-
-    // async function handleCheckToken() {
-    //     checkToken()
-    //         .then(alert)
-    // }
-
     return(
         <>
         <h2>Recipe Detail Page</h2>
         </>
     )
 }
-
-
-//<button onClick={handleCheckToken}>Check When My Login Expires</button>
