909b19e8d8c0ff988a022bdc4e26ee2bec1dbf43
speckoh <speckohart@gmail.com>
2023-03-13 08:44:40 -0400

    styling wip toggle del etc

--- src/pages/CreatePage/CreatePage.js
+++ src/pages/CreatePage/CreatePage.js
@@ -4,168 +4,177 @@ import * as recipeAPI from "../../utilities/recipes-api"
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 import Input from "./Input"
-import "./CreatePage.css"
+// import "./CreatePage.css"
 
-export default function CreatePage({userId}){
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [formValues, setFormValues] = useState([]);
-    const [formStepsValues, setFormStepsValues] = useState([]);
+export default function CreatePage({ userId }) {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [formValues, setFormValues] = useState([{ type: 'text', value: '' }]);
+  const [formStepsValues, setFormStepsValues] = useState([{ type: 'text', value: '' }]);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+  //--------
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value)
+  }
+  //---------
 
-    //--------
-    const handleTitleChange = (event) => {
-        setTitle (event.target.value)
+  // ---------Ingredients
+  const handleChange = (e, index) => {
+    const values = [...formValues];
+    values[index].value = e.target.value;
+    setFormValues(values);
+  };
+  const handleAddField = (e) => {
+    e.preventDefault();
+    const values = [...formValues];
+    values.push({
+      type: "text",
+      value: "",
+    });
+    setFormValues(values);
+    // console.log(formValues.length)
+  };
+  const handleDeleteField = (e, index) => {
+    const values = [...formValues];
+    values.splice(index, 1);
+    setFormValues(values);
+  };
+  //---------
+
+
+  //-------Steps
+  const handleStepChange = (e, index) => {
+    const values = [...formStepsValues];
+    values[index].value = e.target.value;
+    setFormStepsValues(values);
+  };
+  const handleStepAddField = (e) => {
+    e.preventDefault();
+    const values = [...formStepsValues];
+    values.push({
+      type: "text",
+      value: "",
+    });
+    setFormStepsValues(values);
+  };
+  const handleStepDeleteField = (e, index) => {
+    const values = [...formStepsValues];
+    values.splice(index, 1);
+    setFormStepsValues(values);
+  };
+  //---------
+
+
+  async function handleSubmit(event) {
+    try {
+      event.preventDefault()
+
+      console.log(title)
+      console.log(description)
+      console.log(
+        formValues.map((val) => {
+          return val.value;
+        })
+      );
+
+      console.log(
+        formStepsValues.map((val) => {
+          return val.value;
+        })
+      );
+
+      const ingredients = formValues.map((val) => { return val.value })
+      const steps = formStepsValues.map((val) => { return val.value })
+
+      console.log(ingredients)
+      console.log(steps)
+
+      const recipeData = { title, description, ingredients, steps, owner: userId }
+
+      console.log(recipeData)
+      console.log(userId)
+      await recipeAPI.create(recipeData)
+      navigate('/user_recipes')
+      return recipeData
+    } catch (err) {
+      console.error(err)
     }
-    const handleDescriptionChange = (event) => {
-        setDescription(event.target.value)
-    }
-    //---------
+  }
+  return (
+    <div className='create-container'>
+      <form className='create-recipe-form' onSubmit={handleSubmit}>
+        <h2 className='new-recipe-title'>Create New Recipe</h2>
+        <div className='create-flexbox'>
+          <div className='create-left-grid'>
+            <div className='recipe-title-container'>
+              <label className='create-page-labels'>Recipe Title</label>
+              <input id='recipe-title-input' className='create-inputs'
+                type='text'
+                name='title'
+                value={title}
+                onChange={handleTitleChange}
+                placeholder="Title"
+              ></input>
+            </div>
+            <div className='recipe-description-container'>
+              <label className='create-page-labels'>Description</label>
+              <textarea id='description-input'
+                type='text'
+                name='description'
+                value={description}
+                onChange={handleDescriptionChange}
+                placeholder="Description"
+              ></textarea>
+            </div>
+            <div className='recipe-ingredients-container'>
+              <label className='create-page-labels'>Ingredients</label>
+              {formValues.map((obj, index) => (
+                <Input
+                  key={index}
+                  objValue={obj}
+                  onChange={handleChange}
+                  index={index}
+                  deleteField={handleDeleteField}
+                  formValues={formValues}
+                />
+              ))}
+              <button className='create-page-buttons' onClick={handleAddField}>
+                Add ingredient
+              </button>
+            </div>
 
-    // ---------Ingredients
-    const handleChange = (e, index) => {
-        const values = [...formValues];
-        values[index].value = e.target.value;
-        setFormValues(values);
-      };
-      const handleAddField = (e) => {
-        e.preventDefault();
-        const values = [...formValues];
-        values.push({
-          type: "text",
-          value: "",
-        });
-        setFormValues(values);
-      };
-      const handleDeleteField = (e, index) => {
-        const values = [...formValues];
-        values.splice(index, 1);
-        setFormValues(values);
-      };
-      //---------
-
-
-      //-------Steps
-      const handleStepChange = (e, index) => {
-        const values = [...formStepsValues];
-        values[index].value = e.target.value;
-        setFormStepsValues(values);
-      };
-      const handleStepAddField = (e) => {
-        e.preventDefault();
-        const values = [...formStepsValues];
-        values.push({
-          type: "text",
-          value: "",
-        });
-        setFormStepsValues(values);
-      };
-      const handleStepDeleteField = (e, index) => {
-        const values = [...formStepsValues];
-        values.splice(index, 1);
-        setFormStepsValues(values);
-      };
-      //---------
-
-
-    async function handleSubmit(event) {
-        try {
-            event.preventDefault()
-
-            console.log(title)
-            console.log(description)
-            console.log(
-              formValues.map((val) => {
-                return  val.value;
-              })
-            );
-
-            console.log(
-                formStepsValues.map((val) => {
-                  return  val.value;
-                })
-              );
-
-            const ingredients = formValues.map((val) => {return  val.value})
-            const steps = formStepsValues.map((val) => {return  val.value})
-
-            console.log(ingredients)
-            console.log(steps)
-
-            const recipeData = {title, description, ingredients, steps, owner : userId}
-
-            console.log(recipeData)
-            console.log(userId)
-            await recipeAPI.create(recipeData)
-            navigate('/user_recipes')
-            return recipeData
-        } catch(err) {
-            console.error(err)
-        }
-    }
-
-    return(
-        <>
-    <div className="App">
-          <form onSubmit={handleSubmit}>
-          <input
-                    type='text'
-                    name='title'
-                    value={title}
-                    onChange={handleTitleChange}
-                    placeholder="Title"
-                ></input>
-                <input
-                    type='text'
-                    name='description'
-                    value={description}
-                    onChange={handleDescriptionChange}
-                    placeholder="Description"
-                ></input>
-
-            <label>Ingredients:</label>
-            {formValues.map((obj, index) => (
-              <Input
-                key={index}
-                objValue={obj}
-                onChange={handleChange}
-                index={index}
-                deleteField={handleDeleteField}
-              />
-            ))}
-          <div className="center">
-            <button className="add-btn" onClick={handleAddField}>
-              Add new
-            </button>
           </div>
-
-            <label>Steps:</label>
-            {formStepsValues.map((obj, index) => (
-              <Input
-                key={index}
-                objValue={obj}
-                onChange={handleStepChange}
-                index={index}
-                deleteField={handleStepDeleteField}
-              />
-            ))}
-          <div className="center">
-            <button className="add-btn" onClick={handleStepAddField}>
-              Add new
-            </button>
+          <div className='create-right-grid'>
+            <div id='steps-container'>
+              <label className='create-page-labels'>Steps</label>
+              {formStepsValues.map((obj, index) => (
+                <Input
+                  key={index}
+                  objValue={obj}
+                  onChange={handleStepChange}
+                  index={index}
+                  deleteField={handleStepDeleteField}
+                  formStepsValues={formStepsValues}
+                />
+              ))}
+              <button className='create-page-buttons' onClick={handleStepAddField}>
+                Add Step
+              </button>
+            </div>
           </div>
-
-            <button type="submit" className="submit-btn">
+        </div>
+        <div className='create-container'>
+          <button type="submit" className='create-recipe-button'>
             Submit
-            </button>
-          </form>
+          </button></div>
+      </form>
+      {/* <Link to="/user_recipes">Go Back To Your Recipes, curtsey of Greg</Link> */}
     </div>
-      
-        <Link to="/user_recipes">Go Back To Your Recipes, curtsey of Greg</Link>
-        </>
-    )
-
+  )
 }
 
 // https://www.telerik.com/blogs/how-to-programmatically-add-input-fields-react-forms
